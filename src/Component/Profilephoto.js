12617495f511489274e1cbe5a2b3a435218a39a2
sallami <sallami482ahmed@gmail.com>
2023-03-29 13:01:31 +0100

    first commit

---
+++ src/Component/Profilephoto.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ProfilePhoto = () => {
+  return (
+    <img src="myself.jpg" alt="Profile photo" className='photo' width=" 200px"/>
+  );
+};
+
+export default ProfilePhoto;
+
---
+++ src/Component/profile/Adress.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Address = () => {
+  return (
+    <h2>Adress : El Haouaria, Nabeul</h2>
+  );
+};
+
+export default Address;
+
+
+
+
